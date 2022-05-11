13bc467da0b4b9c9dc74eb0f36966d8e43eb72f4
Jennie <jennie1evans@hotmail.com>
2022-05-11 22:24:56 +0000

    Start_with?

--- directory_copy1.rb
+++ directory_copy1.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print_names(students)
-  students.each_with_index do |student, index|
-  puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+  puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("S")
   end
 end
 
